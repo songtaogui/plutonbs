7b235643e09b3ef868d4b68a7faff1ee86916b3f
songtaogui <guisongtao0104@gmail.com>
2022-09-07 14:02:32 +0800

    1st

---
+++ BioGraphs.jl
@@ -0,0 +1,33 @@
+### A Pluto.jl notebook ###
+# v0.19.5
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ c91ac3b0-2e71-11ed-124a-cb8b9650eb5c
+"test"
+
+# ╔═╡ 6b28a8fb-7795-4ff4-9b9c-ef8b044705c5
+
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.8.0"
+manifest_format = "2.0"
+project_hash = "da39a3ee5e6b4b0d3255bfef95601890afd80709"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╠═c91ac3b0-2e71-11ed-124a-cb8b9650eb5c
+# ╠═6b28a8fb-7795-4ff4-9b9c-ef8b044705c5
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
